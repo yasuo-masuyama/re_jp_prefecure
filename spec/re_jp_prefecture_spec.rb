aa3cf2112b6b6a189e0caa719309dbbe57dd804c
Yasuo Masuyama <yasuo.masuyama0526+private@gmail.com>
2026-03-21 16:26:11 +0900

    chore: first commit
    実行コマンド:bundle gem re_jp_prefecture --test=rspec --ci=github --linter=rubocop

---
+++ spec/re_jp_prefecture_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ReJpPrefecture do
+  it "has a version number" do
+    expect(ReJpPrefecture::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(true).to eq(true)
+  end
+end

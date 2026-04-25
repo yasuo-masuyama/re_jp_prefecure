c2af9e6cbfd23f46558645d028b3d2f63fd1519f
Yasuo Masuyama <yasuo.masuyama0526@gmail.com>
2026-04-25 18:39:14 +0900

    refactor: Configクラスのドキュメントコメントを削除
    責務がクラス名・属性名から自明であるため不要なコメントを削除。
    合わせて RuboCop の Style/Documentation を無効化し、
    ドキュメントコメント強制を解除。

--- lib/re_jp_prefecture/config.rb
+++ lib/re_jp_prefecture/config.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 module ReJpPrefecture
-  # カスタムデータの差し替え用設定値を保持するクラス。
-  #
-  # mapping_data には都道府県データ、zip_mapping_data には郵便番号データの
-  # パス または データそのものを設定できる。値が未設定の場合は nil を返す。
   class Config
     attr_accessor :mapping_data, :zip_mapping_data
   end

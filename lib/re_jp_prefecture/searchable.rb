f8556415f7a6632c4d2f8373db3268d7314e4a1f
Yasuo Masuyama <yasuo.masuyama0526@gmail.com>
2026-04-25 19:28:14 +0900

    refactor: Searchable の name 系検索を Symbol#to_proc ブロックで明示化
    public_send による動的フィールド参照をやめ、各 find_by_name* で
    &:name のように対応属性メソッドを直接ブロック渡しする形に変更。
    共通の前方一致ロジックは find_by_string_prefix にまとめる。

--- lib/re_jp_prefecture/searchable.rb
+++ lib/re_jp_prefecture/searchable.rb
@@ -2,27 +2,45 @@
 
 module JpPrefecture
   module Searchable
-    NAME_FIELDS = %i[name name_e name_r name_h name_k].freeze
-
     def find_by_code(value)
       return nil if value.nil?
 
       all.find { |record| record.code == value }
     end
 
-    NAME_FIELDS.each do |field|
-      define_method(:"find_by_#{field}") do |value|
-        return nil if value.nil? || value.to_s.empty?
+    def find_by_name(value)
+      find_by_string_prefix(value, &:name)
+    end
 
-        query = value.to_s.downcase
-        all.find { |record| record.public_send(field).to_s.downcase.start_with?(query) }
-      end
+    def find_by_name_e(value)
+      find_by_string_prefix(value, &:name_e)
+    end
+
+    def find_by_name_r(value)
+      find_by_string_prefix(value, &:name_r)
+    end
+
+    def find_by_name_h(value)
+      find_by_string_prefix(value, &:name_h)
+    end
+
+    def find_by_name_k(value)
+      find_by_string_prefix(value, &:name_k)
     end
 
     def find_by_zip(value)
       return nil if value.nil?
 
       all.find { |record| record.zips.any? { |range| range.cover?(value) } }
+    end
+
+    private
+
+    def find_by_string_prefix(value)
+      return nil if value.nil? || value.to_s.empty?
+
+      query = value.to_s.downcase
+      all.find { |record| yield(record).to_s.downcase.start_with?(query) }
     end
   end
 end
